03ae5325da7592e5999a2f7b7b31dcfba53ddb82
Gary Lewis <indiana_gary@msn.com>
2022-07-26 15:32:18 -0700

    Extracted IColorPicker from ColorPickerBase<T> Added SelectedColorChangedEventArgs Added AttachedTo BindableProperty

---
+++ src/ColorPicker/BaseClasses/SelectedColorChangedEventArgs.cs
@@ -0,0 +1,13 @@
+﻿namespace ColorPicker;
+
+public class SelectedColorChangedEventArgs : EventArgs
+{
+    public Color OldColor { get; }
+    public Color NewColor { get; }
+
+    public SelectedColorChangedEventArgs( Color oldColor, Color newColor )
+    {
+        OldColor = oldColor;
+        NewColor = newColor;
+    }
+}
