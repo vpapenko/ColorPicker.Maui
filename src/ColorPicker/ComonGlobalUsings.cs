d9162c96bdeb8e8aa8021e998b32d3e91c1695df
Gary Lewis <indiana_gary@msn.com>
2022-08-16 15:28:12 -0700

    Moved Orientation to ColorPicker project Add Orientation bindable property to SliderBase Add OrientationConverter

---
+++ src/ColorPicker/ComonGlobalUsings.cs
@@ -0,0 +1,3 @@
+﻿global using System.ComponentModel;
+global using System.Globalization;
+global using ColorPickerMath;
---
+++ src/ColorPicker/Converters/OrientationConverter.cs
@@ -0,0 +1,39 @@
+﻿namespace ColorPicker;
+
+public sealed class OrientationConverter : TypeConverter
+{
+    public override bool CanConvertFrom( ITypeDescriptorContext? context, Type sourceType )
+            => sourceType == typeof( string );
+
+    public override bool CanConvertTo( ITypeDescriptorContext? context, Type? destinationType )
+            => destinationType == typeof( string );
+
+    public override object ConvertFrom( ITypeDescriptorContext? context, CultureInfo? culture, object? value )
+    {
+        var strValue = value?.ToString();
+
+        if ( string.IsNullOrEmpty( strValue ) )
+            return Orientation.Horizontal;
+
+        strValue = strValue.Trim();
+
+        if ( Enum.TryParse( strValue, true, out Orientation result ) )
+            return result;
+
+        throw new InvalidOperationException( string.Format( "Cannot convert \"{0}\" into {1}", strValue, typeof( Orientation ) ) );
+    }
+
+    public override object ConvertTo( ITypeDescriptorContext? context, CultureInfo? culture, object? value, Type destinationType )
+    {
+        if ( value is Orientation result )
+        {
+            if ( result == Orientation.Horizontal )
+                return nameof( Orientation.Horizontal );
+
+            if ( result == Orientation.Vertical )
+                return nameof( Orientation.Vertical );
+        }
+
+        throw new NotSupportedException();
+    }
+}
