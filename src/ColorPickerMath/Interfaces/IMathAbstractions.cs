e4e344e731ac0220a698019fa793eceafe06b3f2
Gary Lewis <indiana_gary@msn.com>
2023-02-03 17:55:38 -0800

    Refactor...

--- src/ColorPickerMath/Interfaces/IMathAbstractions.cs
+++ src/ColorPickerMath/Interfaces/IMathAbstractions.cs
@@ -2,8 +2,8 @@
 
 public interface IMathAbstractions
 {
-    bool IsInActiveArea( PointF point, Color color );
-    PointF FitToActiveArea( PointF point, Color color );
-    PointF ColorToPoint( Color color );
-    Color UpdateColor( PointF point, Color color );
+    bool    IsInActiveArea( PointF point, Color color );
+    PointF  FitToActiveArea( PointF point, Color color );
+    PointF  ColorToPoint( Color color );
+    Color   UpdateColor( PointF point, Color color );
 }
